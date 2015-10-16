471dde1ea742363453b62655da148ed8c36dbf6a
ibagmetgit config --global user.name capuchegit config --global user.name <bagmett@gmail.com>
2015-10-16 15:20:35 -0600

    do

--- tests/simple_test.rb
+++ tests/simple_test.rb
@@ -26,8 +26,8 @@ class SimpleTest < Test::Unit::TestCase
     @browser.goto base_url
 
     @browser.link(text: "Login").when_present.click
-
-    @browser.link (text: "Create a new account").click
+    @browser.link(text: "Create a new account").click
+    
     email_new = ::Faker::Internet.safe_email
     @browser.text_field(name: "spree_user[email]").set email_new
     @browser.text_field(name: "spree_user[first_name]").set 'test'
